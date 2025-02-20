dbbb9debf7e5097f71b9af737a43f869662bc1f8
ankitisgood <111147992+ankitisgood@users.noreply.github.com>
2025-02-20 17:44:24 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://basic-mern-backend.onrender.com"
 
     const [token, setToken] = useState("")
 
